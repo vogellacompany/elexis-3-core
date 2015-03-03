c5262cd66c4add4f3b33d4e10d7418b7d1ca2655
lamann <amann@medevit.at>
2015-03-03 15:13:13 +0100

    [3399] use ISO-8859-1 for all IBM424... encoded HL7 files

--- ch.elexis.core.hl7.v2x/src/ch/elexis/hl7/HL7ReaderFactory.java
+++ ch.elexis.core.hl7.v2x/src/ch/elexis/hl7/HL7ReaderFactory.java
@@ -103,14 +103,14 @@ public enum HL7ReaderFactory {
 		CharsetMatch match = detector.detect();
 		
 		if (match != null) {
-			if (match.getName().equals("IBM424_rtl")) {
+			if (match.getName().contains("IBM424")) {
 				logger.warn("Reading HL7 file " + file.getAbsolutePath()
 					+ " with unsupported encoding " + match.getName()
 					+ " - trying to use ISO-8859-1 instead");
 				return new ByteArrayInputStream(new String(bytes, "ISO-8859-1").getBytes());
 			}
 			logger.info("Reading HL7 file " + file.getAbsolutePath() + " encoded "
-				+ match.getName() + " " + match.getLanguage());
+				+ match.getName() + " language " + match.getLanguage());
 			return new ByteArrayInputStream(match.getString().getBytes());
 		}
 		
